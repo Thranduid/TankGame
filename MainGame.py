1bcda2602c1dae2390a939f8fee72fb3a28db06a
Thrand <Thrand@126.com>
2022-07-08 17:07:54 +0800

    enter

--- MainGame.py
+++ MainGame.py
@@ -1,5 +1,6 @@
 import sys
 import pygame
+import random
 
 
 class Tank():
@@ -72,6 +73,8 @@ class Tank():
         # 将tank 加载到表面
         # 重新设置图片
         self.image = self.images[self.direction]
+        # 设置图片尺寸为30x30
+        self.image = pygame.transform.scale(self.image, (80, 80))
         # 重新加载图片
         MainGame.window.blit(self.image, self.rect)
 
@@ -83,8 +86,48 @@ class MyTank(Tank):
 
 class EnemyTank(Tank):
 
-    def __init__(self) -> None:
-        super().__init__()
+    moveStep = 40
+
+    # 重写敌方类的init()方法 修改图片资源--> 重复代码考虑后续抽象封装
+    def __init__(self, left, top, speed) -> None:
+        super().__init__(left, top)
+        self.images = {
+            'U': pygame.image.load('src\enemy_U.png'),
+            'D': pygame.image.load('src\enemy_D.png'),
+            'L': pygame.image.load('src\enemy_L.png'),
+            'R': pygame.image.load('src\enemy_R.png')
+        }
+        self.direction = self.randomDirection()
+        self.image = self.images[self.direction]
+        self.rect = self.image.get_rect()
+        self.rect.left = left
+        self.rect.top = top
+        self.live = True
+        self.speed = speed
+        self.step = self.moveStep
+
+    # 随机生成敌方类的方向
+    def randomDirection(self):
+        rnum = random.randint(1, 4)
+        if rnum == 1:
+            return 'U'
+        elif rnum == 2:
+            return 'D'
+        elif rnum == 3:
+            return 'L'
+        elif rnum == 4:
+            return 'R'
+        else:
+            print('randonDirection()函数出错')
+
+    def randomMove(self):
+        if self.step == 0:
+            self.direction = self.randomDirection()
+            # random.choice(['U', 'D', 'L', 'R'])
+            self.step = self.moveStep
+        else:
+            self.move()
+            self.step -= 1
 
 
 class Bullet():
@@ -133,8 +176,10 @@ class MainGame():
     RUNNING = True
     window = None
     Tank_P1 = None
-
+    EnemyTankList = []
+    enemyTank_count = 3
     # 初始化
+
     def __init__(self) -> None:
         pygame.init()
         pygame.mixer.init()
@@ -148,13 +193,17 @@ class MainGame():
             (MainGame.HEIGHT, MainGame.WIDTH))
         pygame.display.set_caption('坦克大战v1.0')
         MainGame.Tank_P1 = Tank(230, 450)
+        self.creatEnemyTank()
+
         while MainGame.RUNNING:
 
             # 将窗口背景填充为背景色，如不填充会出现移动飞机时出现残影
             MainGame.window.fill(pygame.Color(0, 0, 0))
 
-            # 加载坦克
+            # 加载我方坦克
             MainGame.Tank_P1.display()
+
+            self.displayALLEnemyTank()
 
             # 监听事件
             self.getEvent()
@@ -167,7 +216,7 @@ class MainGame():
             if pygame.font.get_init():
                 # 字体初始化成功，则将字体小面板绘制在window上
                 MainGame.window.blit(self.fontTips(
-                    "The number of tanks remaining: %d" % 5), (5, 5))
+                    "The number of tanks remaining: %d" % len(MainGame.EnemyTankList)), (5, 5))
             else:
                 # 字体初始化失败，则terminal输出提示
                 print("字体初始化失败！")
@@ -175,8 +224,23 @@ class MainGame():
             # 刷新屏幕
             pygame.display.update()
 
-    # 获取程序运行期间所有事件
+    # 根据数量创建敌方坦克
+    def creatEnemyTank(self):                       # BUG 有可能随机出来的位置出现重叠，需要进行排除
+        for i in range(MainGame.enemyTank_count):
+            left = random.randint(1, 4)
+            top = random.randint(1, 3)
+            speed = random.randint(1, 2)
+            eTank = EnemyTank(left*100, top*50, speed)
+            MainGame.EnemyTankList.append(eTank)
 
+    # 将敌方坦克加载到窗口中
+
+    def displayALLEnemyTank(self):
+        for etank in self.EnemyTankList:
+            etank.randomMove()
+            etank.display()
+
+    # 获取程序运行期间所有事件
     def getEvent(self):
 
         # 获取pygame的全部事件队列，get()返回一个List
@@ -193,7 +257,7 @@ class MainGame():
 
             # 继续判断事件的类型是否是按下按键 如是，则继续判断按下的事件类型
             elif event.type == pygame.KEYDOWN:
-               # 判断是否按下键盘的q键
+                # 判断是否按下键盘的q键
                 if event.key == pygame.K_LEFT:
                     print("go Left")
                     MainGame.Tank_P1.direction = 'L'
